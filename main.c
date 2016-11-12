643e7c3df30e102e1d5492c5385a9452eeb867b9
RotatingFans <patmagauran.j@gmail.com>
2016-11-12 15:58:41 -0500

    Update license info

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 /*
 * ----------------------------------------------------------------------------
-* THE COFFEEWARE LICENSE (Revision 1):
-* <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a coffee in return.
+Created by Rotating Fans 11/12/16
+Licensed under GPLv3
 * -----------------------------------------------------------------------------
 */
 
